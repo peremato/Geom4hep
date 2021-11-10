1fe3ecd71361ba3eea2261af33ba44776f7b3a6c
Pere Mato <pere.mato@cern.ch>
2021-11-10 21:10:17 +0100

    Partial update

--- src/Volume.jl
+++ src/Volume.jl
@@ -1,20 +1,11 @@
-#---Material-----------------------------------------------------------------------
-abstract type AbstractShape{T<:AbstractFloat} end
-abstract type AbstractMaterial{T<:AbstractFloat} end
-
-struct Material{T<:AbstractFloat} <: AbstractMaterial{T}
-    label::String
-    density::T
-end
-Material(label::String, density::Float64) = Material{Float64}(label,density)
-
 
 #---Volume-------------------------------------------------------------------------
 abstract type AbstractPlacedVolume{T<:AbstractFloat} end
+
 struct Volume{T<:AbstractFloat}
     label::String
     shape::Geom4hep.AbstractShape{T}    # Reference to the actual shape
-    material::AbstractMaterial{T}       # Reference to material
+    material::AbstractMaterial         # Reference to material
     daughters::Vector{AbstractPlacedVolume{T}}
 end
 
@@ -28,7 +19,7 @@ contains(pvol::PlacedVolume{T}, p::Point3{T}) where T<:AbstractFloat = inside(pv
 contains(vol::Volume{T}, p::Point3{T}) where T<:AbstractFloat = inside(vol.shape, p) == kInside
 distanceToIn(pvol::PlacedVolume{T}, p::Point3{T}, d::Vector3{T}) where T<:AbstractFloat = distanceToIn(pvol.volume.shape, pvol.transformation * p, pvol.transformation * d)
 
-function Volume(label::String, shape::Geom4hep.AbstractShape{T}, material::AbstractMaterial{T}) where T<:AbstractFloat
+function Volume(label::String, shape::Geom4hep.AbstractShape{T}, material::AbstractMaterial) where T<:AbstractFloat
     Volume{T}(label, shape, material, [])
 end
 
