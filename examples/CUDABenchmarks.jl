4d36a7d821204aa0eda9f0972d7426599cfdc206
Pere Mato <pere.mato@cern.ch>
2022-02-10 10:52:51 +0100

    Updated

--- examples/CUDABenchmarks.jl
+++ examples/CUDABenchmarks.jl
@@ -63,3 +63,33 @@ for T in (Float32, Float64)
     end
 
 end
+
+
+
+
+#=
+<Float32>
+==== Using shape: Box{Float32}(Float32[5.0, 10.0, 15.0]) with 10240 points and directions
+CPU:   399.430 μs (0 allocations: 0 bytes)
+GPU:   10.814 μs (3 allocations: 272 bytes)
+==== Using shape: Tube{Float32}(5.0f0, 10.0f0, 10.0f0, 0.0f0, 6.2831855f0) with 10240 points and directions
+CPU:   407.242 μs (0 allocations: 0 bytes)
+GPU:   21.101 μs (3 allocations: 272 bytes)
+==== Using shape: Cone{Float32}(5.0f0, 10.0f0, 7.0f0, 15.0f0, 10.0f0, 0.0f0, 6.2831855f0) with 10240 points and directions
+CPU:   649.546 μs (0 allocations: 0 bytes)
+GPU:   12.317 μs (3 allocations: 272 bytes)
+
+<Float64>
+==== Using shape: Box{Float64}([5.0, 10.0, 15.0]) with 10240 points and directions
+CPU:   71.975 μs (0 allocations: 0 bytes)
+GPU:   11.859 μs (3 allocations: 272 bytes)
+==== Using shape: Tube{Float64}(5.0, 10.0, 10.0, 0.0, 6.283185307179586) with 10240 points and directions
+CPU:   353.223 μs (0 allocations: 0 bytes)
+GPU:   21.106 μs (3 allocations: 272 bytes)
+==== Using shape: Cone{Float64}(5.0, 10.0, 7.0, 15.0, 10.0, 0.0, 6.283185307179586) with 10240 points and directions
+CPU:   691.625 μs (0 allocations: 0 bytes)
+GPU:   33.259 μs (3 allocations: 272 bytes)
+
+=#
+
+
--- examples/CUDAXRay.jl
+++ examples/CUDAXRay.jl
@@ -2,58 +2,6 @@ using Geom4hep
 using CUDA
 using BenchmarkTools, Test, Printf
 
-function k_generateXRay(result, lower::Point3{T}, pixel::T, state::NavigatorState{T}, view::Symbol) where T<:AbstractFloat
-    nx, ny = size(result)
-    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
-    if i <= nx && j <= ny
-        if view == :x
-            point = Point3{T}(lower[1]+kTolerance(), lower[2]+(i-0.5)*pixel, lower[3]+(j-0.5)*pixel)
-            dir = Vector3{T}(1,0,0)
-        elseif view == :y
-            point = Point3{T}(lower[1]+(j-0.5)*pixel, lower[2]+kTolerance(), lower[3]+(i-0.5)*pixel)
-            dir = Vector3{T}(0,1,0)
-        elseif shift == :z
-            point = Point3{T}(lower[1]+(i-0.5)*pixel, lower[2]+(j-0.5)*pixel, lower[3]+kTolerance())
-            dir = Vector3{T}(0,0,1)
-        end
-        locateGlobalPoint!(state, point)
-        mass::T =  0.0
-        step::T = -1.0
-        while step != 0.0
-            density = currentVolume(state).material.density
-            step = computeStep!(state, point, dir, 1000.)
-            point = point + dir * step
-            mass += step * density
-        end
-        @inbounds result[i,j] = mass
-    end
-    return
-end
-
-function generateXRay(world::Volume{T}, npoints::Number, view::Symbol=:x) where T<:AbstractFloat
-    lower, upper = extent(world.shape)
-    dim = upper - lower
-    if view == :x
-        dim_a, dim_b = dim[2], dim[3]
-    elseif view == :y
-        dim_a, dim_b = dim[3], dim[1]
-    elseif view == :z
-        dim_a, dim_b = dim[1], dim[2]
-    end
-    pixel = round(sqrt(dim_a*dim_b/npoints), sigdigits=3)
-    nx, ny = round(Int, dim_a/pixel), round(Int, dim_b/pixel)
-    state = NavigatorState{Float64}(world)
-    result = CUDA.zeros(nx,ny)
-    threads = (16,16)
-    blocks = cld.((nx,ny),threads)
-    CUDA.@sync @cuda threads=threads blocks=blocks k_generateXRay(result, lower, pixel, state, view)
-    return Array(result)
-end
-
-#-----build and generate image-----------------------------
-world = processGDML("examples/boxes.gdml")
-
 
 @enum ShapeEnum::Int32 kBox kTube kTrd kCone
 function getShapeType(shape::AbstractShape{T})::ShapeEnum where T<:AbstractFloat
@@ -81,6 +29,7 @@ struct CuMaterial{T<:AbstractFloat}
     temperature::T
     Amass::T
 end
+
 CuMaterial{T}(m::Material) where T<: AbstractFloat = CuMaterial{T}(m.density, m.temperature, m.Amass) 
 
 struct CuPlacedVolume{T<:AbstractFloat}
@@ -108,8 +57,6 @@ struct CuModel{T<:AbstractFloat}
             Vector{Cone{T}}())
     end
 end
-
-model = CuModel{Float64}()
 
 function pushObject(indexes::Dict{UInt64, UInt32}, vector::Vector{CUOBJ}, obj::OBJ)::UInt32 where {CUOBJ,OBJ}
     id = objectid(obj)
@@ -144,3 +91,77 @@ function pushVolume(model::CuModel{T}, vol::Volume{T}) where T <: AbstractFloat
     indexes[id]
 end
 
+
+if CUDA.functional()
+    to_gpu(x::AbstractArray) = CuArray(x)
+else
+    to_gpu(x::AbstractArray) = x
+end
+
+end
+function CuGeom(vol::Volume{T}) where T<:AbstractFloat
+    model = CuModel{Float64}()
+    pushVolume(model, vol)
+    return (volumes = to_gpu(model.volumes), 
+            materials = to_gpu(model.materials),
+            daughters = to_gpu(model.daughters),
+            boxes = to_gpu(model.boxes),
+            tubes = to_gpu(model.tubes),
+            trds = to_gpu(model.trds),
+            cones = to_gpu(model.cones))         
+end
+
+
+
+function generateXRay(world::Volume{T}, npoints::Number, view::Symbol=:x) where T<:AbstractFloat
+    lower, upper = extent(world.shape)
+    dim = upper - lower
+    if view == :x
+        dim_a, dim_b = dim[2], dim[3]
+    elseif view == :y
+        dim_a, dim_b = dim[3], dim[1]
+    elseif view == :z
+        dim_a, dim_b = dim[1], dim[2]
+    end
+    pixel = round(sqrt(dim_a*dim_b/npoints), sigdigits=3)
+    nx, ny = round(Int, dim_a/pixel), round(Int, dim_b/pixel)
+    state = NavigatorState{Float64}(world)
+    result = CUDA.zeros(nx,ny)
+    threads = (16,16)
+    blocks = cld.((nx,ny),threads)
+    CUDA.@sync @cuda threads=threads blocks=blocks k_generateXRay(result, lower, pixel, state, view)
+    return Array(result)
+end
+
+
+function k_generateXRay(result, lower::Point3{T}, pixel::T, state::NavigatorState{T}, view::Symbol) where T<:AbstractFloat
+    nx, ny = size(result)
+    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+    if i <= nx && j <= ny
+        if view == :x
+            point = Point3{T}(lower[1]+kTolerance(), lower[2]+(i-0.5)*pixel, lower[3]+(j-0.5)*pixel)
+            dir = Vector3{T}(1,0,0)
+        elseif view == :y
+            point = Point3{T}(lower[1]+(j-0.5)*pixel, lower[2]+kTolerance(), lower[3]+(i-0.5)*pixel)
+            dir = Vector3{T}(0,1,0)
+        elseif shift == :z
+            point = Point3{T}(lower[1]+(i-0.5)*pixel, lower[2]+(j-0.5)*pixel, lower[3]+kTolerance())
+            dir = Vector3{T}(0,0,1)
+        end
+        locateGlobalPoint!(state, point)
+        mass::T =  0.0
+        step::T = -1.0
+        while step != 0.0
+            density = currentVolume(state).material.density
+            step = computeStep!(state, point, dir, 1000.)
+            point = point + dir * step
+            mass += step * density
+        end
+        @inbounds result[i,j] = mass
+    end
+    return
+end
+
+#-----build and generate image-----------------------------
+world = processGDML("examples/boxes.gdml")
